0e0e84cfe56a8635f80d1b1fed3205c22e55f358
James Allen <james.allen@meta.ceo>
2020-08-28 12:50:14 -0500

    feat: initial tailwind css integration

---
+++ apps/ng-transition-issues16/tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.scss',
+  ],
+};
